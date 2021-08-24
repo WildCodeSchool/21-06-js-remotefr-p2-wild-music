c03a8d106034d3a861fcf8edddbd3b14ab27f8c1
Jean-Marie BERNARD <jm11bernard@gmail.com>
2021-08-24 18:45:34 +0200

    Reponsive, fav feature need fixed

--- src/components/Style-mood-section/PlaylistCard.jsx
+++ src/components/Style-mood-section/PlaylistCard.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PlaylistCard.css'
 
-function PlaylistCard({style}) {
+import { TiHeartOutline, TiHeart } from 'react-icons/ti';
+
+function PlaylistCard({style, onAdd, onRemove}) {
+     
+    const [isFavorite, setIsFavorite] = useState(false)
+
+
+    const handleClick = () => {
+        setIsFavorite(!isFavorite)
+    }
+
+    const onClick = () => {
+          handleClick();
+          onAdd();
+         
+      }
+    // onRemove()
+    // const addToFavorite = (style) => {
+    //     setFavorite([...favorite, style]);
+    // }
+
     return (
         <div className='PlaylistCard'>
-            
             <div className='playlist-container'>
                 <img src={style.img} alt={style.name} className="styleImg"/>
                 <div className="card-content">
-                    <h2 className='playlistTitle'>{style.name}</h2>
-                    <div className='quote'>
-                        <img src="https://img.icons8.com/material/48/000000/play-button-circled--v4.png" alt='playerBtn' className='playerBtn'/>
-                        <p><em>{style.quote}</em></p>
-                        <span>{style.quoteBy}</span>
+                    <h2 className='playlistTitle'>{style.name} </h2>
+                    <div className='hoverText'>
+                        <div className="btns">
+                            <img src="https://lh3.googleusercontent.com/proxy/TGqrgWf1W7HY4UPK8Vh3VhK5cN5EtDoad4_E8Q1VUKqvcRBOiccK6d3QZdLZvaOzgXgKe6QMzUSHoPA2vDPLakdZ-hujPL8"
+                            alt='playerBtn' className='playerBtn'/>
+                        </div>
+                        <div className="quote">
+                            <p><em>{style.quote}</em></p>
+                            <span>{style.quoteBy}</span>
+                        </div>
                     </div>
+                    <button className = "favBtn" 
+                    onClick={onClick}> 
+                        {isFavorite ? <TiHeart />  : <TiHeartOutline/>}
+                    </button>
                 </div>
             </div>
         </div>
--- src/components/Style-mood-section/StyleList.jsx
+++ src/components/Style-mood-section/StyleList.jsx
@@ -1,15 +1,55 @@
+import React, {useState} from 'react';
 import PlaylistCard from './PlaylistCard';
 import './StyleList.css'
-let styles = require('../../data/styles')
+const styles = require('../../data/styles')
 
 function StyleList() {
+
+    const [favPlaylist, setFavPlaylist] = useState([]);
+
+    const onAdd = (style) => {
+
+        const exist = favPlaylist.find((x) => x.id === style.id);
+        if (exist) {
+          setFavPlaylist(
+            favPlaylist.map((x) =>
+              x.id === style.id ? { ...exist, qty: exist.qty + 1 } : x
+            )
+          );
+        } else {
+           setFavPlaylist([...favPlaylist, { ...style, qty: 1 }]);
+        }
+    };
+
+    // const onRemove = (styles) => {
+    //     const exist = favPlaylist.find((x) => x.id === styles.id);
+    //     if (exist.qty === 1) {
+    //         setFavPlaylist(favPlaylist.filter((x) => x.id !== styles.id));
+    //     } else {
+    //         setFavPlaylist(
+    //             favPlaylist.map((x) =>
+    //             x.id === styles.id ? { ...exist, qty: exist.qty - 1 } : x
+    //         )
+    //       );
+    //     }
+    //   };onRemove={onRemove}
+
     return (
         <div className="StyleList">
+            <div>
+
+                Cart { ' '}
+                {favPlaylist.length ? (
+                    <button>{favPlaylist.length}</button>
+                ) : (
+                    ' '
+                )}
+            </div>
             <div className="style-container">
                 {styles
                 .map((style) => (
                     <div key={style.id}>
-                        <PlaylistCard style={style}/>
+                        <PlaylistCard style={style} onAdd={onAdd} />
                     </div>
                 ))}
             </div>
